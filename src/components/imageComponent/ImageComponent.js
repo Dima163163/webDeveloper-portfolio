1ecfadb48e42c25b7e594cada7010a27fa841135
Dima163163 <khlopovdima@gmail.com>
2024-05-17 11:20:56 +0400

    fix height imgComponent

--- src/components/imageComponent/ImageComponent.js
+++ src/components/imageComponent/ImageComponent.js
@@ -1,6 +1,8 @@
+import styles from './ImageComponent.module.css';
+
 const ImageComponent = ({ img, imgAvif, imgWebp, title, className }) => {
 	return (
-		<picture>
+		<picture className={styles.picture}>
 			<source srcSet={imgAvif} type="image/avif" />
 			<source srcSet={imgWebp} type="image/webp" />
 			<img src={img} alt={title} className={className} />
