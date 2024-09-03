77456ef83fef70a14852321927b65a3f0ddca9b8
Dima163163 <khlopovdima@gmail.com>
2024-09-03 15:15:12 +0400

    learn-js certificate

--- src/helpers/certificatesList.js
+++ src/helpers/certificatesList.js
@@ -1,4 +1,5 @@
 import certifyHtml from '../img/certificatesLesson/certificate-learnjs-html.jpg';
+import certifyLearnJs from '../img/certificatesLesson/certificate.jpg';
 import certifyGloWeb from '../img/certificatesLesson/diplom-glo-web.jpg';
 import certifyGloJs from '../img/certificatesLesson/diplom-glo-js.jpg';
 import certifyMethed from '../img/certificatesLesson/diplom-methed.jpg';
@@ -15,38 +16,43 @@ export const certificates = [
 	},
 	{
 		id: 1,
+		img: certifyLearnJs,
+		title: 'Сертификат об окончании курса Learnjs JS'
+	},
+	{
+		id: 2,
 		img: certifyMethed,
 		title: 'Сертификат об окончании курса Frontend Methed'
 	},
 	{
-		id: 2,
+		id: 3,
 		img: certifyReactNep,
 		title: 'Сертификат об окончании курса React.Преподаватель Михаил Непомнящий'
 	},
 	{
-		id: 3,
+		id: 4,
 		img: certifyReduxNep,
 		title: 'Сертификат об окончании курса Redux.Преподаватель Михаил Непомнящий'
 	},
 	{
-		id: 4,
+		id: 5,
 		img: certifyReactRedux,
 		title:
 			'Сертификат об окончании курса React, Redux. Преподаватель Богдан Стащук'
 	},
 	{
-		id: 5,
+		id: 6,
 		img: certifyReactReduxLar,
 		title:
 			'Сертификат об окончании курса React, Redux. Преподаватель Антон Ларичев'
 	},
 	{
-		id: 6,
+		id: 7,
 		img: certifyGloWeb,
 		title: 'Сертификат об окончании курса GLO academy Web'
 	},
 	{
-		id: 7,
+		id: 8,
 		img: certifyGloJs,
 		title: 'Сертификат об окончании курса GLO JS лучший студент курса'
 	}
